e2945270aca6ed36e502276a1b148e2a90a2bbae
Gavamot <Gavamot@gmail.com>
2017-11-16 10:43:06 +0500

    st

--- Service/Config.cs
+++ Service/Config.cs
@@ -7,11 +7,7 @@ namespace Service
         public string IpAdress { get; set; }
         public int Port { get; set; }
         public int SocketCount { get; set; }
-        public int DelayMsConnect { get; set; }
-        public int DelayMsRequest { get; set; }
-        public int DelayMsSend { get; set; }
         public int Tries { get; set; }
-        public int BufferSize { get; set; }
 
         public ServicesUpdateTime ServicesUpdateTimeMs { get; set; }
 
@@ -27,12 +23,17 @@ namespace Service
                 IpAdress = "192.168.1.46",
                 Port = 1336,
                 SocketCount = 10,
-                DelayMsConnect = 5000,
-                DelayMsRequest = 1000 * 60,
-                DelayMsSend = 10000 * 60,
                 Tries = 10,
                 DelayMsTries = 3000,
-                ServicesUpdateTimeMs = new ServicesUpdateTime() { BrigedeGetDeviceList = 10000 }
+                ServicesUpdateTimeMs = new ServicesUpdateTime
+                {
+                    BrigedeGetDeviceList = 10000,
+                    BufferSize = 1000000,
+                    DelayMsBetweenReqvest = 50,
+                    DelayMsConnect = 5000,
+                    DelayMsRequest = 1000 * 60,
+                    DelayMsSend = 10000 * 60
+                }
             };
             return res;
         }
@@ -46,5 +47,10 @@ namespace Service
     public class ServicesUpdateTime
     {
         public int BrigedeGetDeviceList { get; set; }
+        public int BufferSize { get; set; }
+        public int DelayMsBetweenReqvest { get; set; }
+        public int DelayMsConnect { get; set; }
+        public int DelayMsRequest { get; set; }
+        public int DelayMsSend { get; set; }
     }
 }
